bb29dd96a4e8f2aa8287d3cdf0cbc192f39e1eeb
geane.araujo <tst.gsa@gmail.com>
2019-04-15 17:29:58 -0400

    criamos a classe usuario

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <?php
 
+// chama arquivo config que chama classe
 require_once("config.php");
 
-$sql = new Sql();
 
-$usuarios = $sql->select("SELECT *FROM tb_usuarios");
+// cria instancia da classe Sql
+//$sql = new Sql();
 
-echo json_encode($usuarios);
+
+// seleciona o método select
+//$usuarios = $sql->select("SELECT *FROM tb_usuarios");
+
+
+//echo json_encode($usuarios);
+
+$root = new Usuario();
+
+$root->loadbyId(3);
+
+echo $root;
 
 ?>
